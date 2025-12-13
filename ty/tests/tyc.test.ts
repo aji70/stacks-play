c191e3b2d4eb1ecd6e17895cf243fffc73f6055d
Ajidokwu Sabo <realjaiboi70@gmail.com>
2025-12-13 21:45:01 +0100

    testing

--- ty/tests/tyc.test.ts
+++ ty/tests/tyc.test.ts
@@ -1,7 +1,13 @@
 
 import { describe, expect, it } from "vitest";
+import { Cl } from "@stacks/transactions";
 
 const accounts = simnet.getAccounts();
+// const alice = accounts.get("wallet_1")!;
+// const bob = accounts.get("wallet_2")!;
+// const charlie = accounts.get("wallet_3")!;
+// const dave = accounts.get("wallet_4")!;
+// const accounts = simnet.getAccounts();
 const address1 = accounts.get("wallet_1")!;
 
 /*
@@ -9,13 +15,15 @@ const address1 = accounts.get("wallet_1")!;
   https://docs.hiro.so/stacks/clarinet-js-sdk
 */
 
-describe("example tests", () => {
+
+
+describe("tycc Contract Tests", () => {
   it("ensures simnet is well initialised", () => {
     expect(simnet.blockHeight).toBeDefined();
   });
 
-  // it("shows an example", () => {
-  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-  //   expect(result).toBeUint(0);
-  // });
+  it("shows an example", () => {
+    const { result } = simnet.callReadOnlyFn("tyc", "is-registered",[Cl.principal(address1)], address1);
+    expect(result).toEqual(Cl.bool(false));
+  });
 });
